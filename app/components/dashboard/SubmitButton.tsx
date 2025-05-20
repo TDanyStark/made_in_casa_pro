4075058ada7f05e6b7e432a292ee605752738fd2
Stark Industries <97692264+TDanyStark@users.noreply.github.com>
2025-05-20 18:03:13 -0500

    feat: add SubmitButton component for logout functionality in SideNav

---
+++ app/components/dashboard/SubmitButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Icons } from "../icons";
+
+export function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-light-bg-2 dark:bg-dark-bg-2 p-3 text-base hover:bg-red-700 hover:text-dark-subtitle md:flex-none md:justify-between md:p-2 md:px-3 cursor-pointer"
+      type="submit"
+      disabled={pending}
+    >
+      <div className="hidden md:block">Sign Out</div>
+
+      {pending ? (
+        <Icons.spinner className="animate-spin" />
+      ) : (
+        <Icons.power strokeWidth={1.2} />
+      )}
+    </button>
+  );
+}
--- app/components/dashboard/sidenav.tsx
+++ app/components/dashboard/sidenav.tsx
@@ -1,14 +1,13 @@
-'use client';
-
 import Link from "next/link";
 import NavLinks from "@/components/dashboard/nav-links";
 import MICLogo from "../icons/MICLogo";
-import { Icons } from "@/components/icons";
 import { logout } from "@/lib/actions/auth";
 import { useFormStatus } from "react-dom";
+import { SubmitButton } from "./SubmitButton";
 
 export default function SideNav() {
-  const { pending } = useFormStatus()
+  const { pending } = useFormStatus();
+  console.log("pending", pending);
   return (
     <aside className="flex h-full flex-col px-3 py-4 md:px-2 bg-primary-foreground">
       <Link
@@ -21,14 +20,7 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-light-bg-2 dark:bg-dark-bg-2 md:block"></div>
         <form action={logout}>
-          <button
-            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-light-bg-2 dark:bg-dark-bg-2 p-3 text-base hover:bg-red-700 hover:text-dark-subtitle md:flex-none md:justify-between md:p-2 md:px-3 cursor-pointer"
-            type="submit"
-            disabled={pending}
-          >
-            <div className="hidden md:block">Sign Out</div>
-            <Icons.power strokeWidth={1.2} />
-          </button>
+          <SubmitButton />
         </form>
       </div>
     </aside>
